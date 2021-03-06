754ea9ff57b565b31be4879f9f4ec70f7f6f2645
kawada <vgnsl94@gmail.com>
2021-03-06 21:00:31 +0900

    テンプレート作成

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    hello
+  <div class="todoListContainer">
+    <div class="heading">
+      <h2 id="title">Todo List</h2>
+      <add-item-form />
+    </div>
+    <list-view />
   </div>
 </template>
 
 <script>
+import AddItemForm from './AddItemForm'
+import ListView from './ListView'
+
 export default {
+  components: {
+    AddItemForm,
+    ListView
+  }
 
 }
 </script>
+
+
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+.heading {
+  background: #e6e6e6;
+  padding: 10px;
+}
+#title {
+  text-align: center;
+}
+</style>
